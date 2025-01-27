221e581588a8d877d00e362b68cf283ddae98451
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-01-27 15:32:24 -0300

    feat(home-page): add page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home-page').then((m) => m.routes),
+  },
+  // Redirect to home when no route matches
+  { path: '**', pathMatch: 'full', redirectTo: '/' },
+];
