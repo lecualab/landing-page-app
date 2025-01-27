27e62390d9774827b1aa54cfed1071a71bcfcf90
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-01-27 16:31:58 -0300

    feat(home-page): add hero component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HomeHeroComponent } from './components/home-hero';
 
 @Component({
-  template: `<p>home works!</p>`,
+  imports: [HomeHeroComponent],
+  template: `<app-home-hero />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {}
