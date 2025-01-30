4f126f0fbf4c0e18f18529dc6ceda63372364bbb
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-01-29 23:22:01 -0300

    feat(home-page): add vision statement component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ContentLayoutComponent } from '@app/components/content-layout';
 import { HomeHeroComponent } from './components/home-hero';
+import { VisionStatementComponent } from './components/vision-statement';
 
 @Component({
-  imports: [HomeHeroComponent],
-  template: `<app-home-hero />`,
+  imports: [
+    HomeHeroComponent,
+    VisionStatementComponent,
+    ContentLayoutComponent,
+  ],
+  template: `
+    <app-home-hero />
+    <app-content-layout>
+      <app-vision-statement />
+    </app-content-layout>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {}
