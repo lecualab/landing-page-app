9d3a54d2e3fc1312148153df34b637e27a7665cf
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-01-30 14:58:03 -0300

    feat(common): add button component

---
+++ src/app/components/common/button/button.component.ts
@@ -0,0 +1,43 @@
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  input,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LoaderComponent } from '../loader';
+
+@Component({
+  selector: 'app-button',
+  imports: [MatButtonModule, MatIconModule, LoaderComponent],
+  template: `
+    <button
+      mat-flat-button
+      extended
+      [disabled]="$disabled() || $isLoading()"
+      class="w-full"
+    >
+      @if ($isLoading()) {
+        <app-loader size="sm" />
+      } @else {
+        @if ($icon(); as icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        <ng-content />
+      }
+    </button>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {
+  readonly $icon = input<string>(undefined, { alias: 'icon' });
+  readonly $disabled = input(false, {
+    alias: 'disabled',
+    transform: booleanAttribute,
+  });
+  readonly $isLoading = input(false, {
+    alias: 'isLoading',
+    transform: booleanAttribute,
+  });
+}
