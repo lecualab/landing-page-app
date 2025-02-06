07f8d41864f11c8b3c645567d5b9ada6c3e7e2c7
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-02-06 20:45:26 -0300

    feat(layout): add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkeletonComponent } from './components/common/skeleton';
+import { FooterComponent } from './components/footer';
 import { ToolbarComponent } from './components/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, FooterComponent, SkeletonComponent],
   template: `
     <header>
       <app-toolbar />
@@ -12,6 +14,13 @@ import { ToolbarComponent } from './components/toolbar';
     <main>
       <router-outlet />
     </main>
+    <footer>
+      @defer {
+        <app-footer />
+      } @placeholder {
+        <app-skeleton height="xl" />
+      }
+    </footer>
   `,
 })
 export class AppComponent {}
