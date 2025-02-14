676b4d640eb06907bb061950d7b4c6dce7d09406
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-02-14 14:45:28 -0300

    feat(contact-page): add page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home-page').then((m) => m.HomePageComponent),
   },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact-page').then((m) => m.ContactPageComponent),
+  },
   // Redirect to home when no route matches
   {
     path: '**',
