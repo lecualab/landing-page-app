3c5a4e268457bd2252aec9cba5016467309b9531
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-02-27 01:24:01 -0300

    feat(common): add lecualina animation component

---
+++ src/app/components/common/lecualina-animation/lecualina-animation.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, timer } from 'rxjs';
+
+const BASE_IMAGE_PATH = 'media/icons/lecualina';
+const LECUALINA_IMAGES = [
+  'lecualina_base.svg',
+  'lecualina_happy.svg',
+  'lecualina_greetings.svg',
+];
+
+@Component({
+  selector: 'app-lecualina-animation',
+  template: `
+    <picture data-testid="lecualina-animation">
+      <img
+        [src]="$currentLecualinaImage()"
+        alt="Greeting from Lecualina"
+        class="size-40 lg:size-50"
+      />
+    </picture>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LecualinaAnimationComponent {
+  protected readonly $currentLecualinaImage = toSignal(
+    timer(0, 800).pipe(
+      map((i) => i % LECUALINA_IMAGES.length),
+      map((nextIndex) => LECUALINA_IMAGES[nextIndex]),
+      map((imageName) => `${BASE_IMAGE_PATH}/${imageName}`),
+    ),
+    { initialValue: `${BASE_IMAGE_PATH}/${LECUALINA_IMAGES[0]}` },
+  );
+}
